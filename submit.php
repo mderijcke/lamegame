0e73f82e864360bf5d53bf16e84ad5f72d2d822b
Vorlot <mitchell@vorlot.com>
2014-12-03 11:41:38 +0100

    Errr

--- submit.php
+++ submit.php
@@ -4,8 +4,15 @@ $pass = '1310Semnb';
 
 try {
 	$dbh = new PDO('mysql:host=127.0.0.1:3306;dbname=lamegame', $user, $pass);
+	if ($dbh == false) {
+		die("Could not connect");
+	}
 
 	$sth = $dbh->query("SELECT `highscore_id`, `highscore_date`, `highscore_score` from `highscore` ORDER BY `highscore_score` ASC");
+	if ($sth == false) {
+		die("Could not query");
+	}
+
 	while ($row = $sth->fetch(PDO::FETCH_NUM)) {
 		echo implode(',', $row) . "\n";
 	}

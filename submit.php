8f8d547f77926e7a582f1dcdcb974aafae0d5706
Vorlot <mitchell@vorlot.com>
2014-12-03 11:33:18 +0100

    Clearly need coffee

--- submit.php
+++ submit.php
@@ -5,7 +5,7 @@ $pass = '1310Semnb';
 try {
 	$dbh = new PDO('mysql:host=127.0.0.1:3306;dbname=lamegame', $user, $pass);
 
-	$sth = $dbh->query("SELECT `highscore_id`, `highscore_date`, `highscore_score` from `highscore` SORT BY `highscore_score` ASC");
+	$sth = $dbh->query("SELECT `highscore_id`, `highscore_date`, `highscore_score` from `highscore` ORDER BY `highscore_score` ASC");
 	while ($row = $sth->fetch(PDO::FETCH_NUM)) {
 		echo implode(',', $row) . "\n";
 	}

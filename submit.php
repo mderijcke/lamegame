6dd87847d93c233273141a7fdb501385d545ad2b
Vorlot <mitchell@vorlot.com>
2014-12-03 11:53:24 +0100

    Update submit.php

--- submit.php
+++ submit.php
@@ -4,12 +4,12 @@ $pass = '1310Semnb';
 
 try {
 	$dbh = new PDO('mysql:host=127.0.0.1:3306;dbname=lamegame', $user, $pass);
-	if ($dbh == false) {
+	if ($dbh === false) {
 		die("Could not connect");
 	}
 
 	$sth = $dbh->query("SELECT `highscore_id`, `highscore_date`, `highscore_score` from `highscore` ORDER BY `highscore_score` ASC");
-	if ($sth == false) {
+	if ($sth === false) {
 		die("Could not query");
 	}
 

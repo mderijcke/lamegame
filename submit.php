682cd18b00f61fdd280f635f231680bcb14d353f
Vorlot <mitchell@vorlot.com>
2014-12-03 11:26:00 +0100

    CSV style highscores

--- submit.php
+++ submit.php
@@ -1,15 +1,18 @@
 <?php
-	$user = 'root';
-	$pass = '1310Semnb';
-	
-	try {
-    $dbh = new PDO('mysql:host=127.0.0.1:3306;dbname=lamegame', $user, $pass);
-    foreach($dbh->query('SELECT * from highscore') as $row) {
-        print_r($row);
-    }
-    $dbh = null;
+$user = 'root';
+$pass = '1310Semnb';
+
+try {
+	$dbh = new PDO('mysql:host=127.0.0.1:3306;dbname=lamegame', $user, $pass);
+
+	$sth = $dbh->query("SELECT `highscore_id`, `highscore_date`, `highscore_score` from `highscore` SORT BY `highscore_asc` ASC");
+	while ($row = $sth->fetch(PDO::FETCH_NUM)) {
+		echo implode(',', $row) . "\n";
+	}
+
+	$dbh = null;
 } catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
+	print "Error!: " . $e->getMessage() . "<br/>";
+	die();
 }
-?>
+

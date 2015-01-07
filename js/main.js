2f53f1c75f2e77c33a23991a4fe4685b6ce49801
BuildTools <unconfigured@null.spigotmc.org>
2015-01-07 19:04:02 +0100

    Added Achievement triggers

--- js/main.js
+++ js/main.js
@@ -26,9 +26,61 @@ $('#target').click(function() {
 		score++;
 		$('#score').text(score);
 	}
+	if (score == 100) {
+
+		var achievement = '#achievement1'
+
+		$(achievement).popover('show')
+
+		setTimeout(function() {
+    	$(achievement).popover('hide')
+		}, 7 * 1000);
+	}
+		if (score == 200) {
+
+		var achievement = '#achievement2'
+
+		$(achievement).popover('show')
+
+		setTimeout(function() {
+    	$(achievement).popover('hide')
+		}, 7 * 1000);
+	}
+		if (score == 300) {
+
+		var achievement = '#achievement3'
+
+		$(achievement).popover('show')
+
+		setTimeout(function() {
+    	$(achievement).popover('hide')
+		}, 7 * 1000);
+	}
+		if (score == 400) {
+
+		var achievement = '#achievement4'
+
+		$(achievement).popover('show')
+
+		setTimeout(function() {
+    	$(achievement).popover('hide')
+		}, 7 * 1000);
+	}
+		if (score == 1000) {
+
+		var achievement = '#achievement5'
+		score = score = 0
+
+		$(achievement).popover('show')
+
+		setTimeout(function() {
+    	$(achievement).popover('hide')
+		}, 7 * 1000);
+	}
 });
 
 $(function() {
 	$('#myTab a:first').tab('show')
 });
+
 
